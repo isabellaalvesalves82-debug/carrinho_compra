a505cfd9caf4075a99b0178321c0f7f4869e791f
Isabella vitória <isabellaalvesalves82@gmail.com>
2025-10-21 10:54:01 -0300

    modificações no javaScript

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ function atualizaCarrinho(){
     listaCarrinho.innerHTML = "";
     total = 0;
 
-    carrinho.foreach((intem, index) => {
+    carrinho.foreach((item, index) => {
         li = document.createElement("li");
-        li.innerHTML = intem.nome + " - " + intem.preco;
-        listaCarrinho.appendChild(li)
-});
+        li.innerHTML = item.nome + " - " + item.preco;
+        listaCarrinho.appendChild(li);
+        total += item.preco;
+    });
+    totalTexto.innerHTML = `Total: R$: ${total.toFixed(2)}`
+}
 
 
 botoesAdicionar.forEach(botao => {
     botao.addEventListener("click", () => {
         const nome = botao.getAttribute("data-nome");
-        const preco = botao.getAttribute("data-preco");
+        const preco = parseFloat(botao.getAttribute("data-preco"));
         
         const items = {nome, preco}
         carrinho.push(items);
